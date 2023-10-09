186bcaa3825b1c2f6f07a127373016b4c8234057
Md. Jahid Hasan Jone <jahid.1802049@bau.edu.bd>
2023-10-10 00:45:30 +0600

    CRD

--- CRD/CRD.R
+++ CRD/CRD.R
@@ -11,10 +11,11 @@ data<-read.csv(file.choose())
 head(data)
 
 #it allows only 6 column at a time
+# divide your columns into groups like the following
 
-d1<-crd(data[2:8],data$Treatments,1)  # 0 for no test, 1 for LSD, 2 for DMRT
-d2<-crd(data[9:12],data$Treatments,1)
+d1 <- crd(data[2:8],data$Treatments,1)  # 0 for no test, 1 for LSD, 2 for DMRT 
+d2 <- crd(data[9:12],data$Treatments,1)
 
+# view results
 d1
-
 d2
